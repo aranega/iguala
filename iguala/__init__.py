cf7fb9bc64494481cb534e968568448c7ee6d064
aranega <vincent.aranega@gmail.com>
2022-11-03 16:42:43 -0600

    Bump version: 0.2.0 → 0.2.1-dev

--- iguala/__init__.py
+++ iguala/__init__.py
@@ -3,4 +3,4 @@ from .matchers import as_matcher, cond, regex
 from .paths import as_path
 
 __ALL__ = ["match", "as_matcher", "as_path", "is_not", "cond", "regex"]
-__version__ = "0.2.0"
+__version__ = "0.2.1-dev"
