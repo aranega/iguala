747fbd6cc33f5ce687ec4002b1bd12ce2a18f884
aranega <vincent.aranega@gmail.com>
2023-04-29 12:46:49 -0600

    Bump version: 0.4.1-dev → 0.5.0-dev

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ packages = ['iguala']
 
 setup(
     name='iguala',
-    version='0.4.1-dev',
+    version='0.5.0-dev',
     description=("Non-linear pattern matching for Python's objects, or rexep-like for objects"),
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
