8f4cf6e3e94586de15efd059f6010bfd05c66ad1
aranega <vincent.aranega@gmail.com>
2023-11-30 10:30:05 -0600

    Add new save_as shortcut

--- iguala/__init__.py
+++ iguala/__init__.py
@@ -1,6 +1,6 @@
 from .helpers import is_not, match
-from .matchers import as_matcher, cond, extended, is_, regex
+from .matchers import as_matcher, cond, extended, is_, regex, save_as
 from .paths import as_path
 
-__ALL__ = ["match", "as_matcher", "as_path", "is_not", "cond", "regex", "extended"]
+__ALL__ = ["match", "as_matcher", "as_path", "is_not", "cond", "regex", "extended", "save_as"]
 __version__ = "0.5.3-dev"
